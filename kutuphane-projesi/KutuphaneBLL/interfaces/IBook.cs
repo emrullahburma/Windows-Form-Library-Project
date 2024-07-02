aa7484f35a399fd4599c4f1944824cf32e5a849c
Emrullah Burma <emrullah.burma@perteknoloji.com>
2024-07-02 16:40:43 +0300

    .

---
+++ kutuphane-projesi/KutuphaneBLL/interfaces/IBook.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace KutuphaneBLL.interfaces
+{
+    public interface IBook
+    {
+        Dictionary<string, object> AddBook(Dictionary<string, object> model);
+        Dictionary<string, object> DeleteBook(Dictionary<string, object> model);
+        Dictionary<string, object> UpdateBook(Dictionary<string, object> model);
+        List<dynamic> GetBook(Dictionary<string, object> model);
+        
+        ///
+        List<dynamic> SearchBook(Dictionary<string, object> model);
+    }
+}
